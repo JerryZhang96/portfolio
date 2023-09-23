a5e24ae6212120e5a596d9ccb8f94a8c93cde732
Jerry <jerry@Jerrys-MacBook-Air.local>
2023-09-23 08:21:10 +0800

    fix: git revert

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -1,33 +1,83 @@
-import React from "react"
+// import React from "react"
+// import { useRouter } from "next/router"
+// import Dropdown from "react-dropdown"
+// import "react-dropdown/style.css"
+
+// const options = [
+//   { value: "en", label: "English" },
+//   { value: "zh", label: "Chinese" },
+// ]
+
+// const LanguageSwitcher = () => {
+//   const router = useRouter()
+//   console.log("router=>", router)
+
+//   const onChange = (e) => {
+//     console.log("e=>", e)
+//     router.push(
+//       {
+//         pathname: router.pathname,
+//         query: router.query,
+//       },
+//       null,
+//       { locale: e.value }
+//     )
+//   }
+
+//   return (
+//     <Dropdown
+//       className="ml-2"
+//       options={options}
+//       onChange={onChange}
+//       value={router.query.locale}
+//     />
+//   )
+// }
+
+// export default LanguageSwitcher
+
 import { useRouter } from "next/router"
 import Dropdown from "react-dropdown"
-import "react-dropdown/style.css"
+import "react-dropdown/style.css" // for basic styling
 
-const options = [
-  { value: "en", label: "English" },
-  { value: "zh", label: "Chinese" },
-]
+import languageDetector from "@/lib/languageDetector"
 
 const LanguageSwitcher = () => {
   const router = useRouter()
 
-  const onChange = (e) => {
-    router.push(
-      {
-        pathname: router.pathname,
-        query: router.query,
-      },
-      null,
-      { locale: e.value }
-    )
+  const options = [
+    { value: "en", label: "English" },
+    { value: "zh", label: "Chinese" },
+  ]
+
+  const handleLanguageChange = (selectedOption) => {
+    const locale = selectedOption.value
+
+    let href = router.asPath
+    let pName = router.pathname
+
+    Object.keys(router.query).forEach((k) => {
+      if (k === "locale") {
+        pName = pName.replace(`[${k}]`, locale)
+        return
+      }
+      pName = pName.replace(`[${k}]`, router.query[k])
+    })
+
+    if (locale) {
+      href = `${pName}`
+      router.push(href) // navigate to the selected language page
+    }
+
+    languageDetector.cache(locale) // cache the selected language
   }
 
   return (
     <Dropdown
       className="ml-2"
       options={options}
-      onChange={onChange}
-      value={router.locale}
+      onChange={handleLanguageChange}
+      value={options.find((option) => option.value === router.query.locale)}
     />
   )
 }
