48c03a68712977dc0daebbef7185952c35c6f827
Jerry <jerry@Jerrys-MacBook-Air.local>
2023-09-23 17:29:22 +0800

    fix: next config base path

--- next.config.js
+++ next.config.js
@@ -2,17 +2,17 @@
 
 const nextConfig = {
   reactStrictMode: true,
-  // basePath: process.env.NODE_ENV === "development" ? "" : "/portfolio",
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      "/": { page: "/" },
-      "/about": { page: "/about" },
-      "/projects": { page: "/projects" },
-    }
-  },
+  basePath: process.env.NODE_ENV === "development" ? "" : "/portfolio",
+  // exportPathMap: async function (
+  //   defaultPathMap,
+  //   { dev, dir, outDir, distDir, buildId }
+  // ) {
+  //   return {
+  //     "/": { page: "/" },
+  //     "/about": { page: "/about" },
+  //     "/projects": { page: "/projects" },
+  //   }
+  // },
   images: {
     unoptimized: true,
   },
